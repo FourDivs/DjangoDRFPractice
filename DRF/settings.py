61802207485f1bfc0e47031e4d1a006e60e82a6d
kothariji <kotharidhruv25@gmail.com>
2021-09-24 16:48:53 +0530

    added django hosts

--- DRF/settings.py
+++ DRF/settings.py
@@ -35,8 +35,10 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'django-drf-basics.herokuapp.com',
+    '127.0.0.1'
+]
 
 # Application definition
 
